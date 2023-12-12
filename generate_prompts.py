8df24c83cf2248814cd98f7c7f02c1bac894ec74
Connie Sun <conniesun2003@gmail.com>
2023-12-12 16:35:05 -0600

    temperature parameter variation

--- generate_prompts.py
+++ generate_prompts.py
@@ -12,6 +12,9 @@ nltk.download('wordnet')
 import Levenshtein
 openai_api_key = "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"
 
+temperature_values = [0.2, 0.5, 0.7, 1.0]
+max_tokens_values = 150
+
 def load_problems(directory):
     problems_data = []
     for filename in sorted(os.listdir(directory)):
@@ -23,13 +26,14 @@ def load_problems(directory):
     return problems_data
 
 # Function to get LLM responses with a maximum character limit
-def get_llm_response(prompt, openai_api_key, max_response_length=150):
+def get_llm_response(prompt, openai_api_key, temperature=0.7, max_tokens=150):
     openai.api_key = openai_api_key
     try:
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=[{"role": "user", "content": prompt}],
-            max_tokens=max_response_length
+            temperature=temperature,
+            max_tokens=max_tokens
         )
         content = response['choices'][0]['message']['content'].strip()
         description, code = extract_code(content)
@@ -68,16 +72,22 @@ def generate_variations(base_prompt):
     return [variation1, variation2, variation3]
 
 def extract_code(content):
-    # This function assumes that the code block is wrapped within triple backticks
+    # Split the content by triple backticks
     parts = content.split('```')
-    if len(parts) >= 3:
-        description = parts[0].strip()  # Text before the code block
-        code = parts[1].strip()         # The code block
-        # It assumes that anything after the first code block is also a description
-        return description, code
-    else:
-        # If no code block is found, return the entire content as description
-        return content, None
+    
+    # Initialize variables for description and code
+    description = ""
+    code = None
+
+    # Process each part
+    for i in range(0, len(parts), 2):
+        if i < len(parts):
+            description += parts[i]
+        if i + 1 < len(parts):
+            code = parts[i + 1].strip()  # Get the code block
+
+    return description.strip(), code
+
 
 
 
@@ -146,8 +156,74 @@ def analyze_prompts(problems_data, openai_api_key):
                     'mean_distance': np.mean(distances) if distances else None
                 }, json_file, indent=4)
 
-
 directory_path = "prompts"
+responses_directory = "responses"
+scores_directory = "scores"
+os.makedirs(responses_directory, exist_ok=True)
+os.makedirs(scores_directory, exist_ok=True)
+
+# Load the prompts from the directory
 loaded_prompts = load_problems(directory_path)
 
-analyze_prompts(loaded_prompts, openai_api_key)
+# Perform experiments with different parameters
+for temperature in temperature_values:
+        print(f"Running experiments with Temperature={temperature}")
+        
+        for base_prompt, filename in loaded_prompts:
+            problem_number = filename.replace('.json', '')
+
+            base_response_data = get_llm_response(base_prompt, openai_api_key, temperature=temperature)
+            
+            if base_response_data['description'] is not None:
+                base_code = base_response_data['code']
+                variations = generate_variations(base_prompt)
+                variation_responses = {}
+
+                for var in variations:
+                    var_response_data = get_llm_response(var, openai_api_key, temperature=temperature)
+                    
+                    if var_response_data['description'] is not None:
+                        var_code = var_response_data['code']
+                        variation_responses[var] = {
+                            'description': var_response_data['description'],
+                            'code': var_code
+                        }
+
+                # Calculate and save scores
+                similarities = []
+                distances = []
+
+                for var, var_response in variation_responses.items():
+                    if base_code and var_response['code']:
+                        sim = calculate_jaccard_similarity(base_code, var_response['code'])
+                        dist = calculate_levenshtein_distance(base_code, var_response['code'])
+                        similarities.append(sim)
+                        distances.append(dist)
+
+                # Save responses to JSON file in the 'responses' folder
+                responses_file_path = os.path.join(responses_directory, f"{problem_number}_T{temperature}.json")
+                with open(responses_file_path, 'w') as json_file:
+                    json.dump({
+                        'base_prompt': base_prompt,
+                        'base_response': base_response_data['description'],
+                        'base_code': base_code,
+                        'variation_responses': variation_responses
+                    }, json_file, indent=4)
+
+                # Save scores to JSON file in the 'scores' folder
+                scores_file_path = os.path.join(scores_directory, f"{problem_number}_T{temperature}.json")
+                with open(scores_file_path, 'w') as json_file:
+                    json.dump({
+                        'prompt': base_prompt,
+                        'similarities': similarities,
+                        'mean_similarity': np.mean(similarities) if similarities else None,
+                        'distances': distances,
+                        'mean_distance': np.mean(distances) if distances else None
+                    }, json_file, indent=4)
+
+# directory_path = "prompts"
+# loaded_prompts = load_problems(directory_path)
+
+
+
+# analyze_prompts(loaded_prompts, openai_api_key)
